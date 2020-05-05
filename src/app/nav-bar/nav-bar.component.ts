96da88b35672fa132e898550c2b8e0fbe2b35ad8
ill-solve-it <mhines33@students.kennesaw.edu>
2020-05-04 23:34:26 -0400

    2 files modified

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'nav-bar',
+  selector: 'navigation',
   templateUrl: './nav-bar.component.html',
   styleUrls: [ './nav-bar.component.css' ]
 })
