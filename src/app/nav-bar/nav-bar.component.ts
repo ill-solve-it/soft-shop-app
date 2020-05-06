143b09bcf406850fb6c769ee47a0622a0146e34f
ill-solve-it <mhines33@students.kennesaw.edu>
2020-05-06 09:48:10 -0700

    1 file modified.

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import {} from '@angular/material/menu';
 import { MatSidenavModule, MatIconModule, MatButtonModule } from '@angular/material';
+const uiModules = [
+  MatSidenavModule,
+  MatIconModule,
+  MatButtonModule
+];
 @Component({
   selector: 'navigation',
   templateUrl: './nav-bar.component.html',
