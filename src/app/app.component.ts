0a10927a3aa2070119c7c4bf9c5b3d53cfbe72c4
ill-solve-it <mhines33@students.kennesaw.edu>
2020-05-08 20:07:16 -0700

    3 files modified. burgr clr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
 import {FlatTreeControl} from '@angular/cdk/tree';
+import {ViewEncapsulation} from '@angular/core';
+
 
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+  encapsulation: ViewEncapsulation.None
+
 })
 export class AppComponent  {
   name = 'Angular';
