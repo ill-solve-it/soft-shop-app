a239dd568fc5f577016e2bc64e5a66f8bae817a8
ill-solve-it <mhines33@students.kennesaw.edu>
2020-05-08 19:42:09 -0700

    3 files modified.

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import {MatMenuTrigger} from '@angular/material/menu'
 import {MatMenuModule} from '@angular/material/menu';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'navigation',
   templateUrl: './nav-bar.component.html',
-  styleUrls: [ './nav-bar.component.css' ]
+  styleUrls: [ './nav-bar.component.css' ],
+  encapsulation: ViewEncapsulation.None
 })
 export class NavBarComponent  {
 
